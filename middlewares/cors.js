19e4ed2a143429532df25a3f2893bc40d25786ff
vitominx <149701427+vitominx@users.noreply.github.com>
2024-05-25 08:56:56 +0300

    изменение в корс

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,21 +1,23 @@
 const allowedCors = [
-    'https://practicum.yandex.ru',
-    'https://students-projects.ru',
-    'http://localhost:3000',
-    'http://localhost:3001'
-]
+  "https://practicum.yandex.ru",
+  "https://students-projects.ru",
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "https://vitominx-frontend.nomoredomainswork.ru",
+];
 
 function cors(req, res, next) {
-    const { origin } = req.headers;
+  const { origin } = req.headers;
 
-    if (allowedCors.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin
-        );
-    }
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-    next();
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+  );
+  next();
 }
 
 module.exports = cors;
-
